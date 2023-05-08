b5acf044feb375e333ad2df92b60e04fc7b40e52
Ioannis Prokopiou <ioannisprokopiou@outlook.com>
2023-05-08 23:46:29 +0300

    markdown update

--- streamlit.py
+++ streamlit.py
@@ -72,7 +72,7 @@ st.divider()
 
 st.text( "Welcome to this step of our data app development project!" )
 st.text("In this step, we will be sharing some interesting visualizations")
-st.text("For more detailed information look our [medium article](https://medium.com/@stoikopoyloyeleonwra/dac2ab8beaf9).")
+st.markdown("For more detailed information look our [medium article](https://medium.com/@stoikopoyloyeleonwra/dac2ab8beaf9).")
 toc.placeholder()
 
 toc.header("Activities visualizations")
